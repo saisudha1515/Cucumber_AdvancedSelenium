77f17cad65e16103f8b4b296ea35ac9f46df191f
LENOVO <LENOVO@DESKTOP-KDI3GK4>
2023-09-16 15:00:01 +0530

    BaseClass is updated

---
+++ src/main/java/genericUtilities/BaseClass.java
@@ -0,0 +1,16 @@
+package genericUtilities;
+
+public class BaseClass {
+	
+	// Java does not support inner method..
+//public static void main(String[] args) {
+//	public static void add() {
+//		System.out.println("Changes");
+//	}
+//}
+	
+	
+	public void add() {
+		System.out.println("Changes");
+	}
+}
